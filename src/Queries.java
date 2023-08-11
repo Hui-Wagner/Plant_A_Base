35132129acc35f29e7c6660c8d82b44053841ebd
Hui Wagner <huiwagne@uw.edu>
2023-08-10 18:52:51 -0700

    Sample Queries test from DB.

--- src/Queries.java
+++ src/Queries.java
@@ -9,17 +9,24 @@ public class Queries {
             ResultSetMetaData metadata = results.getMetaData();
             int columnCount = metadata.getColumnCount();
 
+            // Print column names
+            for (int i = 1; i <= columnCount; i++) {
+                String columnName = metadata.getColumnName(i);
+                System.out.print(columnName + "\t");
+            }
+            System.out.println(); // Print a new line after the column names
+
+            // Print rows
             while (results.next()) {
                 for (int i = 1; i <= columnCount; i++) {
-                    String columnName = metadata.getColumnName(i);
                     String columnValue = results.getString(i);
-                    System.out.println(columnName);
-                    System.out.print(columnValue + ", ");
+                    System.out.print(columnValue + ",");
                 }
                 System.out.println();
             }
         }
     }
+
 }
 
 
